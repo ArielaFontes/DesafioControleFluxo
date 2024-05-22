ee720b4684a6330f930b67b8f407e9cec415f096
ArielaFontes <arielashomo@gmail.com>
2024-05-21 21:42:38 -0300

    Projeto Base: Criando Um Pequeno Sistema Para Validação de Processo Seletivo

---
+++ src/Contador.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Contador {
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+
+        int resultado = parametroDois - parametroUm;
+
+        for (int contador = 1; contador <= resultado; contador++){
+            System.out.println("Contar: " + contador);
+        }
+        
+
+        if (parametroUm > parametroDois)
+        System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+    }
+    
+}
